e2feb417ba13bd3d0cb8eb7408f4097560522083
sirmcclay <mcclay.design@gmail.com>
2020-11-12 11:25:06 -0300

    Configs, variables and types

---
+++ annotations/variables.ts
@@ -0,0 +1,11 @@
+let apples: number = 5;
+console.log(apples);
+
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
