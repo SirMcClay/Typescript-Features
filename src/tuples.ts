fea3b00785956f2c1dba029eaa9eaf89520e99bc
sirmcclay <mcclay.design@gmail.com>
2020-11-13 12:08:23 -0300

    Tuples why dont often used

--- src/tuples.ts
+++ src/tuples.ts
@@ -12,3 +12,11 @@ const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
 // pepsi[0] = 40; ERR
 // pepsi[2] = 'brown'; ERR
+
+// Why tuples isn`t often used
+const carSpecs: [number, number] = [400, 3354];
+// Object is just better
+const carStats = {
+  horsePower: 400,
+  weight: 3354,
+};
