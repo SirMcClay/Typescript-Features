97ad2a6e47822deb5bb3456fb82344b4e8c1b7a3
sirmcclay <mcclay.design@gmail.com>
2020-11-14 20:07:35 -0300

    Interface with partial annotations still work

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -5,6 +5,10 @@ interface Vehicle {
   summary(): string;
 }
 
+interface Reportable {
+  summary(): string;
+}
+
 const oldCivic = {
   name: 'civic',
   year: new Date(),
@@ -14,11 +18,14 @@ const oldCivic = {
   },
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
+// const printVehicle = (vehicle: Vehicle): void => {
+const printSummary = (item: Reportable): void => {
   // console.log(`Name: ${vehicle.name}`);
   // console.log(`Year: ${vehicle.year}`);
   // console.log(`broken? ${vehicle.broken}`);
-  console.log(vehicle.summary());
+  // console.log(vehicle.summary());
+  console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+// printVehicle(oldCivic);
+printSummary(oldCivic);
