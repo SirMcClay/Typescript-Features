d59a8a535276b209a35bbdc86cbbee4024831582
sirmcclay <mcclay.design@gmail.com>
2020-12-02 11:43:30 -0300

    A short glimpse what to do with decorators over express

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,29 +1,32 @@
 import 'reflect-metadata';
+import { isRegularExpressionLiteral } from 'typescript';
 
 const plane = {
 	color: 'red',
 };
 
-@printMetadata
+@controller
 class Plane {
 	color: string = 'red';
 
-	@markFunction('HI THERE')
+	@get('/login')
 	fly(): void {
 		console.log('vrrrrrr');
 	}
 }
 
-function markFunction(secretInfo: string) {
+function get(path: string) {
 	return function (target: Plane, key: string) {
-		Reflect.defineMetadata('secret', secretInfo, target, key);
+		Reflect.defineMetadata('path', path, target, key);
 	};
 }
 
-function printMetadata(target: typeof Plane) {
+function controller(target: typeof Plane) {
 	for (let key in target.prototype) {
-		const secret = Reflect.getMetadata('secret', target.prototype, key);
-		console.log(secret);
+		const path = Reflect.getMetadata('path', target.prototype, key);
+		const middleware = Reflect.getMetadata('middleware', target.prototype, key);
+
+		router.get(path, middleware, target.prototype[key]);
 	}
 }
 
